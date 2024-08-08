734ff06eeeb6a915a17c80abd7b57917b562d502
TaylorD2008 <dorseytaylor4@gmail.com>
2024-08-08 09:55:25 -0400

    UPDATED HEADER

--- src/routes/Questions/QuestionDetails.tsx
+++ src/routes/Questions/QuestionDetails.tsx
@@ -1,3 +1,20 @@
 export default function QuestionDetails(){
-    return (<>Question Details page</>)
+    const mainHeaderStyle = {
+        fontSize: "24px",
+        fontWeight: "bold",
+        color: "darkblue",
+    };
+
+    const subHeaderStyle = {
+        fontSize: "18px",
+        fontWeight: "normal",
+        color: "blue",
+    };
+
+    return (
+    <div>
+    <h1 style={mainHeaderStyle}>Question Details Page</h1>
+    <h2 style={subHeaderStyle}> How to solve...</h2>
+    </div>
+    );
 }
